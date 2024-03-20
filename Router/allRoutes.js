2059afd231dba8de98b14fa29b662843540eab49
someone <someone@someplace.com>
2024-03-20 10:46:57 +0100

    Background différent scss + Page accueil

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,9 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+    new Route("/", "Pré Accueil", "/pages/home.html"),
+    new Route("/accueil", "Accueil", "/pages/accueil.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Futur Site école";
+export const websiteName = "Wiki Shiny Hunting";
