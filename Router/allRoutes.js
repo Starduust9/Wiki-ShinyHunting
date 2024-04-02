01b099111eb84fdb00a7e40cd896b00f760e3336
someone <someone@someplace.com>
2024-04-02 18:21:10 +0200

    allRoutes correction

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
     new Route("/accueil", "Accueil", "/pages/accueil.html"),
     new Route("/bases", "Les Bases", "/pages/bases.html"),
     new Route("/contact", "Contact", "/pages/contact.html", "/js/contact.js"),
-    new Route("/introshinyhunting", "Introduction au Shiny Hunting?", "/pages/introshinyhunting.html"),
+    new Route("/introshinyhunting", "Introduction au Shiny Hunting", "/pages/introshinyhunting.html"),
     new Route("/shinyhuntingjeu", "Le Shiny Hunting dans les jeux", "/pages/shinyhuntingjeu.html"),
     new Route("/siteaide", "Site d'aide", "/pages/siteaide.html"),
     new Route("/streameryoutuber", "Streamer Youtuber", "/pages/streameryoutuber.html"),
