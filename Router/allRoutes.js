d93573b00d2b1ad1899678fcdd35fbe9d4545557
someone <someone@someplace.com>
2025-04-14 20:39:07 +0100

    Modif size logo + Add page Shiny Charm + More Modif

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,6 +8,7 @@ export const allRoutes = [
     new Route("/introshinyhunting", "Introduction au Shiny Hunting", "/pages/introshinyhunting.html"),
     new Route("/shinyhuntingjeu", "Le Shiny Hunting dans les jeux", "/pages/shinyhuntingjeu.html"),
     new Route("/siteaide", "Site d'aide", "/pages/siteaide.html"),
+    new Route("/streameryoutuber", "Streamer Youtuber", "/pages/streameryoutuber.html"),
     new Route("/1G", "1G", "/pages/page-gen/1G.html"),
     new Route("/2G", "2G", "/pages/page-gen/2G.html"),
     new Route("/3G", "3G", "/pages/page-gen/3G.html"),
@@ -18,7 +19,8 @@ export const allRoutes = [
     new Route("/8G", "8G", "/pages/page-gen/8G.html"),
     new Route("/9G", "9G", "/pages/page-gen/9G.html"),
     new Route("/masuda", "Masuda", "/pages/page-gen/masuda.html"),
+    new Route("/charmechroma", "Charme Chroma", "/pages/page-gen/charmechroma.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Wiki Shiny Hunting";
+export const websiteName = "PokéChroma";
